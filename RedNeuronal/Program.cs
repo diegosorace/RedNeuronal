80381f59d818a7dc0d48a5310f3eb7c147a958a4
Diego Sorace <diegoariel.sorace@gmail.com>
2019-07-10 22:36:33 -0300

    Arreglo Mapeo Config

--- RedNeuronal/Program.cs
+++ RedNeuronal/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Json;
 
 namespace RedNeuronal
@@ -16,32 +17,32 @@ namespace RedNeuronal
                 capaEntrada = new CapaEntrada
                 {
                     entradas = new List<double[]>(),
-                    cantidadEntradas = int.Parse(configuration["capaEntrada"]["cantidadEntradas"].ToString()),
-                    valorMaximoEntrada = int.Parse(configuration["capaEntrada"]["cantidadEntradas"].ToString()),
-                    valorMinimoEntrada = int.Parse(configuration["capaEntrada"]["cantidadEntradas"].ToString())
+                    cantidadEntradas = int.Parse(configuration["capaEntrada"]["cantidadEntradas"].ToString(), CultureInfo.InvariantCulture),
+                    valorMaximoEntrada = int.Parse(configuration["capaEntrada"]["valorMaximoEntrada"].ToString(), CultureInfo.InvariantCulture),
+                    valorMinimoEntrada = int.Parse(configuration["capaEntrada"]["valorMinimoEntrada"].ToString(), CultureInfo.InvariantCulture)
                 },
                 capaSalida = new CapaSalida
                 {
                     salidas = new List<double[]>(),
-                    cantidadSalidas = int.Parse(configuration["capaSalida"]["cantidadSalidas"].ToString()),
-                    valorMaximoSalida = int.Parse(configuration["capaSalida"]["valorMaximoSalida"].ToString()),
-                    valorMinimoSalida = int.Parse(configuration["capaSalida"]["valorMinimoSalida"].ToString())
+                    cantidadSalidas = int.Parse(configuration["capaSalida"]["cantidadSalidas"].ToString(), CultureInfo.InvariantCulture),
+                    valorMaximoSalida = int.Parse(configuration["capaSalida"]["valorMaximoSalida"].ToString(), CultureInfo.InvariantCulture),
+                    valorMinimoSalida = int.Parse(configuration["capaSalida"]["valorMinimoSalida"].ToString(), CultureInfo.InvariantCulture)
                 },
                 capaOculta = new CapaOculta
                 {
-                    cantidadCapas = int.Parse(configuration["capaOculta"]["cantidadCapas"].ToString()),
-                    cantidadNeuronasPorCapa = int.Parse(configuration["capaOculta"]["cantidadNeuronasPorCapa"].ToString())
+                    cantidadCapas = int.Parse(configuration["capaOculta"]["cantidadCapas"].ToString(), CultureInfo.InvariantCulture),
+                    cantidadNeuronasPorCapa = int.Parse(configuration["capaOculta"]["cantidadNeuronasPorCapa"].ToString(), CultureInfo.InvariantCulture)
                 },
                 cargarRedNeuronal = bool.Parse(configuration["cargarRedNeuronal"].ToString()),
                 guardarRedNeuronal = bool.Parse(configuration["guardarRedNeuronal"].ToString()),
-                direccionDatosEntrada = configuration["direccionDatosEntrada"].ToString(),
-                direccionDatosSalida = configuration["direccionDatosSalida"].ToString(),
-                direccionRedNeuronal = configuration["direccionRedNeuronal"].ToString(),
-                alpha = double.Parse(configuration["alpha"].ToString()),
-                errorMax = double.Parse(configuration["errorMax"].ToString()),
-                iteracionesDeEntrenamiento = int.Parse(configuration["iteracionesDeEntrenamiento"].ToString())
+                direccionDatosEntrada = configuration["direccionDatosEntrada"],
+                direccionDatosSalida = configuration["direccionDatosSalida"],
+                direccionRedNeuronal = configuration["direccionRedNeuronal"],
+                alpha = double.Parse(configuration["alpha"].ToString(), CultureInfo.InvariantCulture),
+                errorMax = double.Parse(configuration["errorMax"].ToString(), CultureInfo.InvariantCulture),
+                iteracionesDeEntrenamiento = int.Parse(configuration["iteracionesDeEntrenamiento"].ToString(), CultureInfo.InvariantCulture)
             };
-
+            
             Red red = new Red
             (
                 redConfig
